d37aa1fc745a6490859d3e2fa886962c05b1bc10
Seth Baron <seth@syncpointcreative.com>
2026-04-08 09:34:19 -0600

    fix: bust build cache, add vercelignore

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  // force clean install by busting Vercel's build cache
+  generateBuildId: async () => 'verb-' + Date.now(),
+}
 module.exports = nextConfig
